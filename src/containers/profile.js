e68bb8427d7b8dac970dd59fa8a9f8934caf5a85
Mitchell Williams <mscottwilliams1994@gmail.com>
2019-10-24 14:24:39 -0500

    reset

--- src/containers/profile.js
+++ src/containers/profile.js
@@ -2,21 +2,21 @@ import React, { Component } from "react";
 import "../App.css";
 import "react-bootstrap";
 
-const usersURL = "http://localhost:3000/users/"
+const usersURL = "http://localhost:3000/users/";
 class Profile extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       username: window.localStorage.getItem("username"),
       email: window.localStorage.getItem("email"),
       wallet: 0
-    }
+    };
   }
 
   handleChange = event => {
     event.persist();
-    this.setState({[event.target.name]: event.target.value})
-  }
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
   handleSubmit = event => {
     event.preventDefault();
@@ -26,30 +26,35 @@ class Profile extends Component {
       method: "PATCH",
       headers: {
         "content-type": "application/json",
-        "accept": "application/json",
-        "Authorization": `Bearer ${window.localStorage.getItem("token")}`
+        accept: "application/json",
+        Authorization: `Bearer ${window.localStorage.getItem("token")}`
       },
       body: JSON.stringify(formInputs)
-    }
+    };
     fetch(`${usersURL}${window.localStorage.getItem("id")}`, configObj)
-    .then(res => res.json())
-    .then(json => {
-      console.log(json)
-      window.localStorage.setItem("username", json.user.username)
-      window.localStorage.setItem("email", json.user.email)
-      this.setState({username: json.user.username, email: json.user.email})
-    })
-    .catch(error => console.log(error))
-  }
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        window.localStorage.setItem("username", json.user.username);
+        window.localStorage.setItem("email", json.user.email);
+        this.setState({ username: json.user.username, email: json.user.email });
+      })
+      .catch(error => console.log(error));
+  };
 
-  componentDidMount(){
-    let configObj = {method: "GET", headers: {"Authorization": `Bearer ${window.localStorage.getItem("token")}`}}
+  componentDidMount() {
+    let configObj = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${window.localStorage.getItem("token")}`
+      }
+    };
     fetch(`${usersURL}${window.localStorage.getItem("id")}`, configObj)
-    .then(res => res.json())
-    .then(json => {
-      this.setState({wallet: json.user.wallet})
-    })
-    .catch(error => console.log(error))
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ wallet: json.user.wallet });
+      })
+      .catch(error => console.log(error));
   }
   render() {
     return (
@@ -58,19 +63,24 @@ class Profile extends Component {
 
         {<h3> Email: {window.localStorage.getItem("email")}</h3>}
 
-<<<<<<< HEAD
-        {<h4>Wallet Amount: {window.localStorage.getItem("wallet")}</h4>}
-=======
         {<h4>Wallet Amount: {this.state.wallet}</h4>}
         <form onSubmit={event => this.handleSubmit(event)}>
           <label>Edit username</label>
-          <input type="text" name="username" onChange={event => this.handleChange(event)} value={this.state.username} />
+          <input
+            type="text"
+            name="username"
+            onChange={event => this.handleChange(event)}
+            value={this.state.username}
+          />
           <label>Edit email</label>
-          <input type="text" name="email" onChange={event => this.handleChange(event)} value={this.state.email} />
+          <input
+            type="text"
+            name="email"
+            onChange={event => this.handleChange(event)}
+            value={this.state.email}
+          />
           <input type="submit" value="Edit user" />
         </form>
-
->>>>>>> origin/master
       </div>
     );
   }
